00af1d2bb9941b6d214524d1645d50ccd888b215
Kabir Ghai <74025064+CypherGuy@users.noreply.github.com>
2024-12-12 15:29:54 +0000

    Create text_node_to_html_node function to handle different text types

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -64,3 +64,18 @@ class ParentNode(HTMLNode):
             props_html = self.props_to_html()
 
             return f"<{self.tag}{props_html}>{children_html}</{self.tag}>"
+
+
+def text_node_to_html_node(text_node):
+    if text_node.text_type == TextType.BOLD:
+        return LeafNode("b", text_node.text)
+    elif text_node.text_type == TextType.ITALIC:
+        return LeafNode("i", text_node.text)
+    elif text_node.text_type == TextType.LINK:
+        return LeafNode("a", text_node.text, {"href": text_node.url})
+    elif text_node.text_type == TextType.IMAGE:
+        return LeafNode("img", None, {"src": text_node.url})
+    elif text_node.text_type == TextType.CODE:
+        return LeafNode("code", text_node.text)
+    else:
+        return LeafNode(None, text_node.text)
